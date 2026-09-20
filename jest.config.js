014508c52f0a5b4b8369790f518aa459f49ccf21
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-09-21 05:59:29 +0900

    fix: jest.config.jsのフォーマットを修正

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': ['@swc/jest']
-  },
-  setupFilesAfterEnv: ['aws-cdk-lib/testhelpers/jest-autoclean'],
+    testEnvironment: "node",
+    roots: ["<rootDir>/test"],
+    testMatch: ["**/*.test.ts"],
+    transform: {
+        "^.+\\.tsx?$": ["@swc/jest"],
+    },
+    setupFilesAfterEnv: ["aws-cdk-lib/testhelpers/jest-autoclean"],
 };
